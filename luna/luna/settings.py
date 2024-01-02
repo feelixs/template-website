f060a42bf23864db0311be6c6805e548a3117678
Michael Felix <mikemh@uri.edu>
2024-01-02 16:11:12 -0500

    Update settings.py

--- luna/luna/settings.py
+++ luna/luna/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = os.path.join(BASE_DIR, 'luna', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'luna', 'collected_static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
